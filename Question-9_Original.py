65096607f8ab982f902abcddd06794f918734fad
Hemanth C <33805839+hchimata@users.noreply.github.com>
2018-01-14 17:13:26 +0530

    Add files via upload
    Need to improve this answer

---
+++ Question-9_Original.py
@@ -0,0 +1,10 @@
+"""Need to improve this method"""
+
+for a in range(1, 1000):
+    for b in range(a, 1000):
+        c = 1000 - a - b
+        if c > 0:
+            if c * c == a * a + b * b:
+                print(a,b,c)
+                print(a * b * c)
+                break
